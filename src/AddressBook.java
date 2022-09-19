6f68f5746d9d6d116a2769c48248a53114dcd6de
Varun Sindwani <varunsindwani87@gmail.com>
2022-09-19 13:40:45 -0400

    added objs and functions

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,16 +4,20 @@ public class AddressBook {
 
     ArrayList<BuddyInfo> buddys = new ArrayList<BuddyInfo>();
 
-    void addBuddy(ArrayList<BuddyInfo> buddys, BuddyInfo b) {
-
+    void addBuddy(BuddyInfo b) {
+        buddys.add(b);
     }
 
-    void removeBuddy(ArrayList<BuddyInfo> buddys, BuddyInfo b) {
-
+    void removeBuddy(int index) {
+        buddys.remove(index);
     }
 
     public static void main(String[] args) {
         System.out.println("Address Books");
+        BuddyInfo b2 = new BuddyInfo("Homer", "somehwere","123456869");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(b2);
+        addressBook.removeBuddy(0);
     }
 
 
