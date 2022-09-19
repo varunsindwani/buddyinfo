62f74acfa3ef6d105ff0921ede0f53719627d7ea
Varun Sindwani <varunsindwani87@gmail.com>
2022-09-19 13:36:07 -0400

    changed text

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    ArrayList<BuddyInfo> buddys = new ArrayList<BuddyInfo>();
+
+    void addBuddy(ArrayList<BuddyInfo> buddys, BuddyInfo b) {
+
+    }
+
+    void removeBuddy(ArrayList<BuddyInfo> buddys, BuddyInfo b) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Books");
+    }
+
+
+}
